bf5ca7ec4d8817a12dd5b77ad0c96ac842444e11
skirt-owner <skirtsfield@gmail.com>
2023-10-27 10:50:34 +0300

    Fix response headers

--- src/middlewares/morgan.middleware.js
+++ src/middlewares/morgan.middleware.js
@@ -4,7 +4,7 @@ const { v4: uuidv4 } = require('uuid');
 const logger = require('../utils/logger');
 
 morgan.token('requestId', (req, res) => {
-    return req.requestId;
+    return res.getHeader('X-Request-ID');
 });  
 
 const stream = {
--- src/middlewares/requestId.middleware.js
+++ src/middlewares/requestId.middleware.js
@@ -1,7 +1,8 @@
 const { v4: uuidv4 } = require('uuid');
 
 const requestIdMiddleware = (req, res, next) => {
-    req.requestId = uuidv4();
+    const requestId = uuidv4();
+    res.setHeader('X-Request-ID', requestId);
     next();
 };
 
